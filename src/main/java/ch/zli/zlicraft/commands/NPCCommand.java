bffd26a94fe3b5b76360d91438d0e52c7448d54c
Lezurex <lenny.angst@easyid.ch>
2021-05-19 13:42:07 +0200

    Add NMS stuff

---
+++ src/main/java/ch/zli/zlicraft/commands/NPCCommand.java
@@ -0,0 +1,18 @@
+package ch.zli.zlicraft.commands;
+
+import net.minecraft.server.v1_16_R3.EntityPlayer;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class NPCCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+        }
+        return false;
+    }
+}
--- src/main/java/ch/zli/zlicraft/listener/JoinListener.java
+++ src/main/java/ch/zli/zlicraft/listener/JoinListener.java
@@ -1,6 +1,5 @@
 package ch.zli.zlicraft.listener;
 
-import net.kyori.adventure.text.Component;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -9,7 +8,7 @@ public class JoinListener implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        event.joinMessage(Component.text("§a+ §1" + event.getPlayer().getName() + " §7ist beigetreten."));
+        event.setJoinMessage("§a+ §1" + event.getPlayer().getName() + " §7ist beigetreten.");
     }
 
 }
